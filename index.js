6c97fbe25038fc382a9b5d9776a0e1fff684fea9
Replit user <>
2023-12-07 07:11:15 +0000

    readline

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-let fs = require("fs");
-// let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+// readline
+const rl = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-let input = fs.readFileSync("input.txt").toString().split('\n');
-
-console.log(input);
+let input = [];
+rl.on("line", function (line) {
+  // 콘솔 입력창에서 줄바꿈(Enter)를 입력할 때마다 호출
+  input.push(line);
+}).on("close", function () {
+  // 콘솔 입력 창에서 Ctrl+C, Ctrl+D를 입력하면 호출(입력 종료)
+  console.log("input");
+  process.exit();
+});
