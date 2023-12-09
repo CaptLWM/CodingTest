caf227eb18bcdb4f6b33247a011809142eca4bad
Replit user <wonmin275@gmail.com>
2023-12-09 12:38:29 +0000

    mergeSort

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 let test = Array.from({length:30000}, ()=>Math.floor(Math.random()*1000));
 
-function bubbleSort(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    let minIndex = i; // rkwkd wkrdms dnjsthdml dlseprtm
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[minIndex] > arr[j]) {
-        minIndex = j;
-      }
-    }
-    //스와프(swap)
-    let temp = arr[i];
-    arr[i] = arr[minIndex];
-    arr[minIndex] = temp;
+function merge(arr, left, mid, right) {
+  let i = left;
+  let j = mid + 1;
+  let k = left; // 결과 배열의 인덱스
+  while (i <= mid && j <= right) {
+    if (arr[i] <= arr[j]) sorted[k++] = arr[i++];
+    else sorted[k++] = arr[j++];
+  }
+  // 왼쪽 배열에 대한 처리가 다 끝난 경우
+  if (i > mid) {
+    for (; j <= right; j++) sorted[k++] = arr[j];
+  }
+  // 오른쪽 배열에 대한 처리가 다 끝난 경우
+  else {
+    for (; i <= mid; i++) sorted[k++] = arr[i];
+  }
+  // 정렬된 배열 결과를 원본 배열에 반영
+  for (let x = left; x <= right; x++) {
+    arr[x] = sorted[x];
   }
 }
 
+
 startTime = new Date().getTime();
-bubbleSort(test);
+merge(test);
 endTime= new Date().getTime();
 
 console.log('time', endTime-startTime)
