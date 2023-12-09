934da0082b8247071e73b0bab4910bfa951d65f8
Replit user <wonmin275@gmail.com>
2023-12-09 14:36:00 +0000

    12.9

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
 let fs = require("fs");
 let input = fs.readFileSync("input.txt").toString().split("\n");
 
-let n = input[0];
-let k = input[1].split(' ').map(Number);
-let temp = [...new Set(k)]
-temp.sort((a,b)=>a-b);
-
-let myMap=new Map();
-for (let i=0; i<temp.length; i++ ) {
-  myMap.set(temp[i],i);
+let n = input[0].split('').sort((a,b)=>b-a);
+let answer ='';
+for(let i=0; i<n.length;i++) {
+  answer+=n[i];
 }
 
-let answer2 = '';
-for (x of k) {
-  answer2+=myMap.get(x) + ' ';
-}
-
-console.log(k, temp, answer2);
+console.log(answer)
--- setMap.js
+++ setMap.js
@@ -2,18 +2,18 @@ let fs = require("fs");
 let input = fs.readFileSync("input.txt").toString().split("\n");
 
 let n = input[0];
-let k = input[1].split(' ').map(Number);
-let temp = [...new Set(k)]
-temp.sort((a,b)=>a-b);
+let k = input[1].split(" ").map(Number);
+let temp = [...new Set(k)];
+temp.sort((a, b) => a - b);
 
-let myMap=new Map();
-for (let i=0; i<temp.length; i++ ) {
-  myMap.set(temp[i],i);
+let myMap = new Map();
+for (let i = 0; i < temp.length; i++) {
+  myMap.set(temp[i], i);
 }
 
-let answer2 = '';
+let answer2 = "";
 for (x of k) {
-  answer2+=myMap.get(x) + ' ';
+  answer2 += myMap.get(x) + " ";
 }
 
 console.log(k, temp, answer2);
