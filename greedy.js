f22774ef4943eccf1f8d4757f76f9f0414ee175e
Replit user <wonmin275@gmail.com>
2023-12-11 13:34:20 +0000

    신입사원 #1946

--- greedy.js
+++ greedy.js
@@ -105,3 +105,45 @@ while (start <= target) {
 if(flag) console.log(result);
 else console.log(-1);
 
+// 못품
+let fs = require("fs");
+let input = fs.readFileSync("input.txt").toString().split("\n");
+
+let number = Number(input[0]);
+let a = 0;
+let b = 0;
+
+while (b<=number) {
+  a+=1;
+
+  b+=a;
+}
+console.log(a-1)
+
+// 모르겠음 1946
+
+let fs = require("fs");
+let input = fs.readFileSync("input.txt").toString().split("\n");
+
+let testCase = Number(input[0]); // 테스트 케이스
+let line = 1;
+for (let tc = 0; tc < testCase; tc++) {
+  n = Number(input[line]); // 케이스별 인원수
+  let arr = [];
+  for (let i = line + 1; i <= line + n; i++) {
+    let data = input[i].split(" ").map(Number);
+    arr.push(data);
+  }
+  arr.sort((x, y) => x[0] - y[0]); // x 순위를 기준으로 오름차순
+  let count = 0;
+  let minvalue = 100001; // 가장낮은순위 + 1
+  for (let [x, y] of arr) {
+    if (y < minvalue) {
+      // 순위값이 가장 작다면 카운트
+      minvalue = y;
+      count += 1;
+    }
+  }
+  console.log(count);
+  line += n + 1;
+}
--- index.js
+++ index.js
@@ -1,22 +1,27 @@
-// 못품
+// 모르겠음
+
 let fs = require("fs");
 let input = fs.readFileSync("input.txt").toString().split("\n");
 
-let start = Number(input[0].split(' ')[0]);
-let target = Number(input[0].split(' ')[1]);
-let flag = false;
-let result = 1;
-
-while (start <= target) {
-  if(start===target) {
-    flag=true;
-    break;
+let testCase = Number(input[0]); // 테스트 케이스
+let line = 1;
+for (let tc = 0; tc < testCase; tc++) {
+  n = Number(input[line]); // 케이스별 인원수
+  let arr = [];
+  for (let i = line + 1; i <= line + n; i++) {
+    let data = input[i].split(" ").map(Number);
+    arr.push(data);
   }
-  if(target %2 ===0) target = parseInt(target/2);
-  else if(target %10===1) target=parseInt(target/10);// 1의 자릿수가 1인 경우 => 1의 자릿수를 제거하기 위함
-  else break;
-  result ++;
+  arr.sort((x, y) => x[0] - y[0]); // x 순위를 기준으로 오름차순
+  let count = 0;
+  let minvalue = 100001; // 가장낮은순위 + 1
+  for (let [x, y] of arr) {
+    if (y < minvalue) {
+      // 순위값이 가장 작다면 카운트
+      minvalue = y;
+      count += 1;
+    }
+  }
+  console.log(count);
+  line += n + 1;
 }
-if(flag) console.log(result);
-else console.log(-1);
-
