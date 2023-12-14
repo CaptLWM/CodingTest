4fc5cba09f59200512d686520d6a8a7486e74c79
Replit user <wonmin275@gmail.com>
2023-12-14 07:43:57 +0000

    상한액

--- index.js
+++ index.js
@@ -1,34 +1,55 @@
 let fs = require("fs");
 let input = fs.readFileSync("input.txt").toString().split("\n");
 
-// 정렬된 순서를 유지하면서 배열에 삽입할 가장 왼쪽 인덱스 반환
-function lowerBound(arr, target, start, end) {
-  while (start < end) {
-    let mid = parseInt((start + end) / 2);
-    if (arr[mid] >= target) end = mid; // 최대한 왼쪽으로 이동
-    else start = mid + 1;
+let n = Number(input[0].split(" ")[0]); // 지방의 수
+let arr = input[1].split(" ").map(Number); // 요청별 예산
+let m = Number(input[2]); // 배정된 총 예산
+
+let start = 1; // 이진탐색을 위한 시작점(start) 끝점(end) 설정
+let end = arr.reduce((a, b) => Math.max(a, b));
+
+let result = 0;
+while (start <= end) {
+  let mid = parseInt((start + end) / 2); // 현재의 중간점(상한액)
+  let total = 0; // 배정된 예산 총액 계산
+  for (x of arr) {
+    // 각 지방에서 요청한 예산을 하나씩 확인하며
+    total += Math.min(mid, x); // 예산 배정
   }
-  return end;
-}
-
-// 정렬된 순서를 유지하면서 배열에 삽입할 가장 오른쪽 인덱스 반환
-function upperBound(arr, target, start, end) {
-  while (start < end) {
-    let mid = parseInt((start + end) / 2);
-    if (arr[mid] > target) end = mid; // 타겟보다 큰값들중 가장 왼쪽
-    else start = mid + 1; // 최대한 오른쪽으로 이동
+  if (total <= m) {
+    // 조건을 만족한다면
+    result = mid;
+    start = mid + 1;
+  } else {
+    end = mid - 1;
   }
-  return end;
+  console.log(mid, total, start, end)
 }
+console.log(result);
 
-// countByRange();
-function countByRange(arr, leftValue, rightValue) {
-  // 유의 : lowerBound와 upperBound는 end 변수의 값을 배열의 길이로 설정
-  let rightIndex = upperBound(arr, rightValue, 0, arr.length);
-  let leftIndex = lowerBound(arr, leftValue, 0, arr.length);
-  return rightIndex - leftIndex;
-}
+// function parametric(arr, total) {
+//   let n = arr.length;
+//   let avg = parseInt(total / n);
+//   let a = 0;
+//   for (let i = 0; i < n; i++) {
+//     if (arr[i] <= avg) {
+//       a += arr[i];
+//       n--;
+//     }
+//   }
 
-let arr = [1, 2, 3, 3, 3, 3, 3, 4, 4, 5, 8, 9];
-console.log(countByRange(arr, 4, 4));
-console.log(countByRange(arr, -1, 3));
+//   let limitPrice = parseInt((total - a) / n);
+//   console.log(limitPrice);
+// }
+
+// let n = Number(input[0]); // 요청 수
+// let tc = input[1].split(" ").map(Number); // 요청별 예산
+// let total = Number(input[2]); // 배정된 총 예산
+
+// let sum = 0;
+// for (let i = 0; i < n; i++) {
+//   sum += tc[i];
+// }
+
+// if (total > sum) console.log(Math.max(...tc));
+// else parametric(tc, total);
