eba3c06ced71e7b2077bc3df971fd1e83fab133d
Replit user <wonmin275@gmail.com>
2023-12-15 13:25:01 +0000

    modify 16562

--- baekjoon/16562.js
+++ baekjoon/16562.js
@@ -6,7 +6,7 @@ let input = fs.readFileSync("input.txt").toString().split("\n");
 let [n, m] = input[0].split(" ").map(Number); // 1부터 N가지 자연수 중에서 중복없이 M개를 고른 조합
 let arr = []; // 조합을 계산하고자 하는 원소(element)가 담긴 배열
 for (let i = 1; i <= n; i++) arr.push(i);
-let visited = new Array(n).fill(false); // 각 원소 인덱스 별 방문 여부
+// let visited = new Array(n).fill(false); // 각 원소 인덱스 별 방문 여부
 let selected = [];
 
 let answer = "";
@@ -24,11 +24,9 @@ function dfs(arr, depth, start) {
     // start 지점부터 하나씩 원소 인덱스를 확인
     // if(visited[i]) continue; // 중복을 허용하지 않으므로 이미 처리된 원소라면 무시
     selected.push(i); // 현재 원소 선택
-    visited[i] = true; // 현재 원소 방문 처리
 
     dfs(arr, depth + 1, i); // 중복조합이므로 재귀함수 호출 시 인덱스 유지
     selected.pop(); // 현재원소 선택 취소
-    visited[i] = false;
   }
 }
 
