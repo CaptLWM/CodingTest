fc86fd384d091df03225f551172cfd8c44731a8c
Replit user <wonmin275@gmail.com>
2023-12-18 13:14:49 +0000

    10971

--- index.js
+++ index.js
@@ -1,34 +1,55 @@
 let fs = require("fs");
-let input = fs.readFileSync('input.txt').toString().split("\n");
+let input = fs.readFileSync("input.txt").toString().split("\n");
 
-// N과 M 15652
-
-let [n, m] = input[0].split(' ').map(Number); // 1부터 N가지 자연수 중에서 중복없이 M개를 고른 조합
-let arr = []; // 조합을 계산하고자 하는 원소(element)가 담긴 배열
-for (let i=1; i<=n; i++) arr.push(i);
-let visited = new Array(n).fill(false); // 각 원소 인덱스 별 방문 여부
-let selected = [];
-
-let answer ='';
-function dfs(arr, depth, start) {
-  if(depth === m) { // 모든 조합을 확인하는 부분
-    // console.log('selected',selected)
-    let result = [];
-    for (let i of selected) result.push(arr[i]);
-    for (let x of result) answer += x + ' '; // 계산된 조합을 실질적으로 처리하는 부분
-    answer += '\n';
-    return;
-  }
-  for(let i=start; i<arr.length;i++) { // start 지점부터 하나씩 원소 인덱스를 확인
-    // if(visited[i]) continue; // 중복을 허용하지 않으므로 이미 처리된 원소라면 무시
-    selected.push(i); // 현재 원소 선택
-    visited[i] = true; // 현재 원소 방문 처리
-
-    dfs(arr, depth+1, i); // 중복조합이므로 재귀함수 호출 시 인덱스 유지
-    selected.pop();// 현재원소 선택 취소
-    visited[i]=false;
-  }
+// 외판원 10971
+/** 
+  1번노드에서 시작한다고 가정하고 고민 시작 => 어쨌든 1번을 지나야 하기 때문
+*/
+let n = Number(input[0]);
+let graph = [];
+for (let i = 0; i <= n; i++) {
+  graph.push([0]); // 인접행렬 방식을 사용하기위해 인덱스 0 자리에 임의의 값을 넣어준것
+  // [ [ 0 ], [ 0 ], [ 0 ], [ 0 ], [ 0 ] ]
 }
 
-dfs(arr, 0,0)
-console.log(answer)
+console.log(graph);
+for (let i = 1; i <= n; i++) {
+  line = input[i].split(" ").map(Number);
+  for (let j = 0; j < n; j++) {
+    graph[i].push(line[j]);
+  }
+}
+let visited = new Array(11).fill(false); // 방문 처리 배열
+let result = []; // 순열 정보
+let minValue = 1e9;
+// console.log(graph);
+function dfs(depth) {
+  if (depth === n - 1) {
+    let totalCoast = 0; // 현재 순열에 대한 처리
+    let cur = 1; // 1번 노드에서 출발
+    for (let i = 0; i < n - 1; i++) {
+      // 현재 순열에 따라서 노드 이동
+      let nextNode = result[i]; // 다음 이동 노드까지의 비용 계산
+      let cost = graph[cur][nextNode];
+      if (cost === 0) return; // 이동불가는 무시
+      totalCoast += cost; // 비용 더하고
+      cur = nextNode; // 노드 이동
+    }
+    // 마지막 노드에서 1로 돌아와야함
+
+    let cost = graph[cur][1];
+    if (cost === 0) return; // 이동불가능은 무시
+    totalCoast += cost; // 비용 더하고
+    minValue = Math.min(minValue, totalCoast); // 경로의 최소 비용 저장
+  }
+  for (let i = 2; i <= n; i++) {
+    if (visited[i]) continue;
+    result.push(i); // 방문처리
+    visited[i] = true;
+    dfs(depth + 1); // 재귀 함수 호출
+    result.pop(); // 방문처리 해제
+    visited[i] = false;
+  }
+}
+dfs(0);
+console.log(minValue);
