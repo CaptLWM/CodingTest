540eec29e752c2cd985e52325356439f77ca21c6
Replit user <wonmin275@gmail.com>
2023-12-21 07:38:50 +0000

    2961

--- index.js
+++ index.js
@@ -1,55 +1,41 @@
 let fs = require("fs");
 let input = fs.readFileSync("input.txt").toString().split("\n");
 
-// 외판원 10971
-/** 
-  1번노드에서 시작한다고 가정하고 고민 시작 => 어쨌든 1번을 지나야 하기 때문
-*/
+// 2961
+
 let n = Number(input[0]);
-let graph = [];
-for (let i = 0; i <= n; i++) {
-  graph.push([0]); // 인접행렬 방식을 사용하기위해 인덱스 0 자리에 임의의 값을 넣어준것
-  // [ [ 0 ], [ 0 ], [ 0 ], [ 0 ], [ 0 ] ]
-}
-
-console.log(graph);
+let arr = [];
 for (let i = 1; i <= n; i++) {
-  line = input[i].split(" ").map(Number);
-  for (let j = 0; j < n; j++) {
-    graph[i].push(line[j]);
-  }
+  let [x, y] = input[i].split(" ").map(Number);
+  arr.push([x, y]);
 }
-let visited = new Array(11).fill(false); // 방문 처리 배열
-let result = []; // 순열 정보
-let minValue = 1e9;
-// console.log(graph);
-function dfs(depth) {
-  if (depth === n - 1) {
-    let totalCoast = 0; // 현재 순열에 대한 처리
-    let cur = 1; // 1번 노드에서 출발
-    for (let i = 0; i < n - 1; i++) {
-      // 현재 순열에 따라서 노드 이동
-      let nextNode = result[i]; // 다음 이동 노드까지의 비용 계산
-      let cost = graph[cur][nextNode];
-      if (cost === 0) return; // 이동불가는 무시
-      totalCoast += cost; // 비용 더하고
-      cur = nextNode; // 노드 이동
-    }
-    // 마지막 노드에서 1로 돌아와야함
 
-    let cost = graph[cur][1];
-    if (cost === 0) return; // 이동불가능은 무시
-    totalCoast += cost; // 비용 더하고
-    minValue = Math.min(minValue, totalCoast); // 경로의 최소 비용 저장
+let visited = new Array(n).fill(false); // 방문 처리 배열
+let result = []; // 순열 정보
+let answer = 1e9;
+// console.log(graph);
+function dfs(depth, start) {
+  if (depth >= 1) {
+    // 현재 조합에 대하여 결과 계산
+    let totalX = 1;
+    let totalY = 0;
+    for (let i of result) {
+      // 인덱스(index)를 하나씩 확인하며
+      let [x, y] = arr[i];
+      totalX *= x;
+      totalY += y;
+    }
+    answer = Math.min(answer, Math.abs(totalX - totalY));
   }
-  for (let i = 2; i <= n; i++) {
+  for (let i = start; i < n; i++) {
+    // 모든 조합 계산
     if (visited[i]) continue;
-    result.push(i); // 방문처리
-    visited[i] = true;
-    dfs(depth + 1); // 재귀 함수 호출
-    result.pop(); // 방문처리 해제
+    visited[i] = true; // 방문처리
+    result.push(i);
+    dfs(depth + 1, i + 1); // 재귀 함수 호출
     visited[i] = false;
+    result.pop(); // 방문처리 해제
   }
 }
-dfs(0);
-console.log(minValue);
+dfs(0, 0);
+console.log(answer);
